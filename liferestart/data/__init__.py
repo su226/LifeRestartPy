99825e044047e9afaa8eacfa2ddab6d5c2fbc375
su226 <thesu226@outlook.com>
2024-11-11 16:25:56 +0800

    fix: 指定encoding

--- liferestart/data/__init__.py
+++ liferestart/data/__init__.py
@@ -9,23 +9,23 @@ from ..struct.event import Event
 from ..struct.talent import Talent
 
 _dir = os.path.dirname(os.path.abspath(__file__))
-with open(f"{_dir}/age.json") as f:
+with open(f"{_dir}/age.json", encoding="utf-8") as f:
   AGE: Dict[int, Weights] = {
     int(i["age"]): parse_weights(i["event"]) for i in json.load(f).values()
   }
-with open(f"{_dir}/talents.json") as f:
+with open(f"{_dir}/talents.json", encoding="utf-8") as f:
   TALENT: Dict[int, Talent] = {
     (parsed := Talent.parse(talent)).id: parsed for talent in json.load(f).values()
   }
-with open(f"{_dir}/events.json") as f:
+with open(f"{_dir}/events.json", encoding="utf-8") as f:
   EVENT: Dict[int, Event] = {
     (parsed := Event.parse(event)).id: parsed for event in json.load(f).values()
   }
-with open(f"{_dir}/achievement.json") as f:
+with open(f"{_dir}/achievement.json", encoding="utf-8") as f:
   ACHIEVEMENT: Dict[int, Achievement] = {
     (parsed := Achievement.parse(achievement)).id: parsed for achievement in json.load(f).values()
   }
-with open(f"{_dir}/character.json") as f:
+with open(f"{_dir}/character.json", encoding="utf-8") as f:
   CHARACTER: Dict[int, PresetCharacter] = {
     (parsed := PresetCharacter.parse(character)).id: parsed for character in json.load(f).values()
   }
